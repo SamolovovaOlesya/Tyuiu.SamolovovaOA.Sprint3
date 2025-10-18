438d663c3a153078368cd5b13936fc5910b3fd8f
Olesya Samolovova <Tyuiu.SamolovovaOA@outlook.com>
2025-10-18 13:31:35 +0500

    Task1

---
+++ Tyuiu.SamolovovaOA.Sprint3.Task1.V14.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.SamolovovaOA.Sprint3.Task1.V14.Lib;
+namespace Tyuiu.SamolovovaOA.Sprint3.Task1.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestGetSumSeries()
+        {
+            DataService ds = new DataService();
+            double value = 0.7;
+            int startValue = 1;
+            int stopValue = 14;
+            double res = ds.GetSumSeries(value, startValue, stopValue);
+            double wait = 3.546;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.SamolovovaOA.Sprint3.Task1.V14/Program.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.SamolovovaOA.Sprint3.Task1.V14.Lib;
+namespace Tyuiu.SamolovovaOA.Sprint3.Task1.V14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            double value = 0.7;
+            int startValue = 1;
+            int stopValue = 14;
+
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                *");
+            Console.WriteLine("*******************************************************************");
+            Console.WriteLine($"t = {value}");
+            Console.WriteLine("*******************************************************************");
+
+            double result = ds.GetSumSeries(value, startValue, stopValue);
+
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                      *");
+            Console.WriteLine("*******************************************************************");
+            Console.WriteLine($"Сумма ряда = {result}");
+            Console.WriteLine("*******************************************************************");
+            Console.ReadKey();
+        }
+    }
+}
